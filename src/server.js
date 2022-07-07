63b171a3948963280a3c0982731f5349b1da6997
nsharma-uk <n.sharmauk711@gmail.com>
2022-07-07 17:29:29 +0100

    added init function to server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const connection = require ("./config/connection")
 
+
+// import sequelize connection
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +13,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+const init = async () => {
+    try {
+      // connect to DB
+      await connection.sync({ force: false });
+  
+      // server listen on PORT
+      app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+      });
+    } catch (error) {
+      console.log(`[ERROR]: Failed to start server | ${error.message}`);
+    }
+  };
+  
+  init();
