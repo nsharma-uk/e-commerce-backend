59c3dd6a643c720f8ac53d5e9de32a69d838236f
nsharma-uk <n.sharmauk711@gmail.com>
2022-07-07 12:20:20 +0100

    create db, models & connection

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,15 +1,5 @@
-require('dotenv').config();
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
 
-const Sequelize = require('sequelize');
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
-
-module.exports = sequelize;
+-- CREATE DATABASE
+CREATE DATABASE ecommerce_db;
